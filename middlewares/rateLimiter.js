70a0ddb4e019d0d484ca83774a73bf8ba36c9cc8
rayan <rayanhassan200691@gmail.com>
2026-03-14 14:53:06 +0500

    add rate limiting and user quota based on user plan type

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const aiLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 1 day
-  max: 1,
+  max: 30,
   keyGenerator: (req) => req.user.id,
   message: "Daily AI limit reached (30/day)",
 });
